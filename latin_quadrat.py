0d6056e108c31e8854fe8b041bf3f962f63351f3
Sergey R <slog18122018@gmail.com>
2024-01-27 14:44:22 +0300

    new files

--- latin_quadrat.py
+++ latin_quadrat.py
@@ -1,22 +1,45 @@
-#
-# max = 8
-len_array = 8
+a = 10000
 
-array = 'ABCDEFGH'
-
-c = 0
-
-flag = [False for i in range(len_array)]
-
-def rotation (arr, box):
-    global c
-    for i in arr:
-        if len((box + i)) == len_array:
-            print(box)
-            c = c + 1
+def p_to_n(n):
+    f = 1
+    for i in range(1, (n+1)):
+        f *= i
+    x = f
+    for j in range(1, (n+1)):
+        if j & 1:
+            m = -1
         else:
-            rotation((arr.replace(i, '')), (box + i))
+            m = 1
+        for k in range(1, (j+1)):
+            m *= k
+        x += (f // m)
+    return x
+
+b = p_to_n(a)
+print(f"{a} ---> {b}")
 
 
-rotation(array[:len_array], '')
-print(c)
+
+
+# #
+# # max = 8
+# len_array = 8
+
+# array = 'ABCDEFGH'
+
+# c = 0
+
+# flag = [False for i in range(len_array)]
+
+# def rotation (arr, box):
+#     global c
+#     for i in arr:
+#         if len((box + i)) == len_array:
+#             print(box)
+#             c = c + 1
+#         else:
+#             rotation((arr.replace(i, '')), (box + i))
+
+
+# rotation(array[:len_array], '')
+# print(c)
