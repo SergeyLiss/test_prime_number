37878d7d6b80f7a283c83d60afcfb03e60a8eda2
Sergey R <slog18122018@gmail.com>
2024-02-12 21:33:15 +0300

    добавлено по 512 файлов prime_8 & prime_9

--- tp_array.py
+++ tp_array.py
@@ -1,37 +1,53 @@
 # Формирование списка простых
 import multiprocessing as mps
 from datetime import datetime as dt
+import time
 
 class LPN: # List Prime Number
-    list_prime = [2, 3, 5, 7]
 
     def __init__(self) -> None:
-        self.cpu_count = mps.cpu_count() - 1
-        # self.bar = mps.Barrier(self.cpu_count)
+        self.cpu_count = mps.cpu_count()
+        self.list_prime = mps.Manager().list()
+        self.iter = mps.Manager().Value('i', 3)
+        self.lock = mps.Manager().RLock()
+        self.semafor = mps.Manager().BoundedSemaphore(1)
+        self.number = 0
+        self.processes = []
         pass
 
     def __call__(self, number):
-        with mps.Pool(self.cpu_count) as p:
-            for i in range(9,number,2):
-                p.apply_async(self.prime_search, args=(i,), callback=self.list_add)
-            p.close()
-            p.join() 
+        self.number = number
+        self.list_prime.append(3)
+        with mps.Pool(self.cpu_count) as pool:
+            while self.iter.value < self.number:
+                pool.apply_async(self.list_add, args=(self.prime_search(),))
+                self.iter.value += 2
+                
+            
+            pool.close()
+            pool.join()
+        
         pass
 
     def list_add(self, prime):
         if prime != 0:
-            self.list_prime.append(prime)
+            self.semafor.acquire()
+            temp = self.list_prime
+            temp.append(prime)
+            self.list_prime = temp
+            # self.list_prime.append(prime)
+            self.semafor.release()
         pass
 
-    def prime_search(self, prjme):
+    def prime_search(self):
         for i in self.list_prime:
-            if (prjme % i) == 0:
+            if (self.iter.value % i) == 0:
                 return 0
-        return prjme
+        return self.iter.value
 
 
 if __name__ == "__main__":
-    size = 1000
+    size = 100
     start = dt.now()
     test = LPN()
     test(size)
